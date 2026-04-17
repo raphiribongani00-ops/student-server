784c73e49ed93b2938bb970bab5dc7dee6b7369e
raphiribongani00-ops <raphiribongani00@gmail.com>
2026-04-17 14:01:47 +0200

    Update idCard.js

--- script-Files/idCard.js
+++ script-Files/idCard.js
@@ -44,7 +44,7 @@ async function addIdCard() {
     submitButton.textContent = 'Submitting...';
 
     try {
-        const response = await fetch('http://localhost:3000/api/save-id-card', {
+        const response = await fetch('http://https://student-server.onrender.com/api/save-id-card', {
             method: 'POST',
             body: formData
         });
