c2751dcaa707074f50a19fe519ee496529ddf9ed
raphiribongani00-ops <raphiribongani00@gmail.com>
2026-04-17 13:59:11 +0200

    Update devices.js

--- script-Files/devices.js
+++ script-Files/devices.js
@@ -42,7 +42,7 @@ async function addDevice() {
     submitButton.textContent = 'Submitting...';
 
     try {
-        const response = await fetch('http://localhost:3000/api/save-device', {
+        const response = await fetch('http://https://student-server.onrender.com/api/save-device', {
             method: 'POST',
             body: formData
         });
