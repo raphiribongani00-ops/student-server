d2e665037ef0348bfe1e2e0baf81ff5c278531b2
raphiribongani00-ops <raphiribongani00@gmail.com>
2026-04-17 14:02:49 +0200

    Update otherItems.js

--- script-Files/otherItems.js
+++ script-Files/otherItems.js
@@ -42,7 +42,7 @@ async function addOtherItem() {
     submitButton.textContent = 'Submitting...';
 
     try {
-        const response = await fetch('http://localhost:3000/api/save-item', {
+        const response = await fetch('http://https://student-server.onrender.com/api/save-item', {
             method: 'POST',
             body: formData
         });
